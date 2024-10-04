b8c154edf1d557d191af68728ae05700fe07158d
Bruno Rodrich <bruno.rodrich@mail.huji.ac.il>
2024-10-04 20:59:27 +0300

    seventh

---
+++ Week8/Day5/miniproject/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RandomQuote from './RandomQuote';  // Importar el componente de la cita aleatoria
+import './App.css';  // Puedes ajustar el estilo si es necesario
+
+function App() {
+  return (
+    <div className="App">
+      <RandomQuote />
+    </div>
+  );
+}
+
+export default App;
